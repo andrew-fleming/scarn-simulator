b0f6bf59fa3118efb936d70308a35d8f8d47d84b
andrew <fleming-andrew@protonmail.com>
2025-08-28 03:00:57 -0300

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { createSimulator } from './factory/createSimulator.js';
+export { AbstractSimulator } from './core/AbstractSimulator.js';
+export { ContractSimulator } from './core/ContractSimulator.js';
+export { StateManager } from './core/StateManager.js';
+
+// Export types
+export type {
+  IContractSimulator,
+  ContextlessCircuits,
+  ExtractPureCircuits,
+  ExtractImpureCircuits,
+} from './types/index.js';
+export type { BaseSimulatorOptions } from './types/Options.js';
+export type { SimulatorConfig } from './factory/SimulatorConfig.js';
