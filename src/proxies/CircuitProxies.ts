ed986f822c1dbe977ccdae07ed828f53cc1b089d
andrew <fleming-andrew@protonmail.com>
2025-08-28 22:58:14 -0300

    improve sig and type safety in createCircuitProxies

--- src/proxies/CircuitProxies.ts
+++ src/proxies/CircuitProxies.ts
@@ -13,17 +13,20 @@ import type {
  */
 export function createCircuitProxies<
   P,
-  ContractType extends { circuits: any; impureCircuits: any },
+  ContractType extends {
+    circuits: Record<PropertyKey, unknown>;
+    impureCircuits: Record<PropertyKey, unknown>;
+  },
 >(
   contract: ContractType,
   getContext: () => CircuitContext<P>,
   getCallerContext: () => CircuitContext<P>,
   updateContext: (ctx: CircuitContext<P>) => void,
-  createPureProxy: <C extends object>(
+  createPureProxy: <C extends Record<PropertyKey, unknown>>(
     circuits: C,
     context: () => CircuitContext<P>,
   ) => ContextlessCircuits<C, P>,
-  createImpureProxy: <C extends object>(
+  createImpureProxy: <C extends Record<PropertyKey, unknown>>(
     circuits: C,
     context: () => CircuitContext<P>,
     updateContext: (ctx: CircuitContext<P>) => void,
@@ -35,11 +38,14 @@ export function createCircuitProxies<
   return {
     get circuits() {
       if (!pureProxy) {
-        pureProxy = createPureProxy(contract.circuits, getContext);
+        pureProxy = createPureProxy(
+          contract.circuits as ExtractPureCircuits<ContractType>,
+          getContext,
+        );
       }
       if (!impureProxy) {
         impureProxy = createImpureProxy(
-          contract.impureCircuits,
+          contract.impureCircuits as ExtractImpureCircuits<ContractType>,
           getCallerContext,
           updateContext,
         );
