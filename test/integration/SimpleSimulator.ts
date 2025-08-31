4564f37a174db5d1d5085f519fe39e0441498ce2
andrew <fleming-andrew@protonmail.com>
2025-08-30 21:14:59 -0300

    improve test structure, add simple sim

---
+++ test/integration/SimpleSimulator.ts
@@ -0,0 +1,39 @@
+import {
+  ledger,
+  Contract as SimpleContract,
+} from '../fixtures/artifacts/Simple/contract/index.cjs';
+import {
+  SimplePrivateState,
+  SimpleWitnesses,
+} from '../fixtures/mocks/witnesses/SimpleWitnesses';
+import { createSimulator, BaseSimulatorOptions } from '../../src/index';
+
+/**
+ * Base simulator
+ */
+const SimpleSimulatorBase = createSimulator({
+  contractFactory: (witnesses) => new SimpleContract<SimplePrivateState>(witnesses),
+  defaultPrivateState: () => SimplePrivateState,
+  contractArgs: () => [],
+  ledgerExtractor: (state) => ledger(state),
+  witnessesFactory: () => SimpleWitnesses(),
+});
+
+/**
+ * Simple Simulator
+ */
+export class SimpleSimulator extends SimpleSimulatorBase {
+  constructor(
+    options: BaseSimulatorOptions<SimplePrivateState, ReturnType<typeof SimpleWitnesses>> = {}
+  ) {
+    super(options);
+  }
+
+  public setVal(n: bigint) {
+    this.circuits.impure.setVal(n);
+  }
+
+  public getVal(): bigint {
+    return this.circuits.impure.getVal();
+  }
+}
